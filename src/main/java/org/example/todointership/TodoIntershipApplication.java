e6a6feee1f28399a319dfdb487132e72a850063a
Hamad tarawa <tourt2031@gmail.com>
2026-08-19 15:49:53 +0300

    Stage 0: hello server

---
+++ src/main/java/org/example/todointership/TodoIntershipApplication.java
@@ -0,0 +1,13 @@
+package org.example.todointership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoIntershipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoIntershipApplication.class, args);
+    }
+
+}
